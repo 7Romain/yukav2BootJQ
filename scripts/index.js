b8781508edfec3137ab723a327b2332af0895ef4
7Romain <osiiriis@ymail.com>
2022-09-30 22:15:31 +0200

    Co-authored-by: shui@live.fr <shui@live.fr>

--- scripts/index.js
+++ scripts/index.js
@@ -328,12 +328,12 @@ const afficherCaracteristiques = function (resultat) {
     let texte = "";
     if (resultat.products[0]["product_name_fr"]) {
         texte +=
-            "<span class= 'gras' >Nom : </span>" +
+            "<span class= 'gras'>Nom : </span>" +
             resultat.products[0]["product_name_fr"] +
             "<br/><br/>";
     } else if (resultat.products[0]["product_name_en"]) {
         texte +=
-            "<span class= 'gras' >Nom Générique : </span>" +
+            "<span class= 'gras'>Nom Générique : </span>" +
             resultat.products[0]["product_name_en"] +
             "<br/><br/>";
     } else {
@@ -342,58 +342,61 @@ const afficherCaracteristiques = function (resultat) {
 
     if (resultat.products[0]["brands_imported"]) {
         texte +=
-            "<span class= 'gras' >Marque : </span>" +
+            "<span class= 'gras'>Marque : </span>" +
             resultat.products[0]["brands_imported"] +
             "<br/><br/>";
     } else if (resultat.products[0]["brands"]) {
-        texte += "Marque : " + resultat.products[0]["brands"] + "<br/><br/>";
+        texte +=
+            "<span class= 'gras'> Marque : </span>" +
+            resultat.products[0]["brands"] +
+            "<br/><br/>";
     }
 
     if (resultat.products[0]["quantity"]) {
         texte +=
-            "<span class= 'gras' >Quantité : </span>" +
+            "<span class= 'gras'>Quantité : </span>" +
             resultat.products[0]["quantity"] +
             "<br/><br/>";
     }
 
     if (resultat.products[0]["packaging_text_fr"]) {
         texte +=
-            "<span class= 'gras' >Conditionnement : </span>" +
+            "<span class= 'gras'>Conditionnement : </span>" +
             resultat.products[0]["packaging_text_fr"] +
             "<br/><br/>";
     }
 
     if (resultat.products[0]["categories_old"]) {
         texte +=
-            "<span class= 'gras' >Catégories : </span>" +
+            "<span class= 'gras'>Catégories : </span>" +
             resultat.products[0]["categories_old"] +
             "<br/><br/>";
     }
 
     if (resultat.products[0]["preparation_fr"]) {
         texte +=
-            "<span class= 'gras' >Préparation : </span>" +
+            "<span class= 'gras'>Préparation : </span>" +
             resultat.products[0]["preparation_fr"] +
             "<br/><br/>";
     }
 
     if (resultat.products[0]["traces_imported"]) {
         texte +=
-            "<span class= 'gras' >Traces : </span>" +
+            "<span class= 'gras'>Traces : </span>" +
             resultat.products[0]["traces_imported"] +
             "<br/><br/>";
     }
 
     if (resultat.products[0]["stores"]) {
         texte +=
-            "<span class= 'gras' >Magasins : </span>" +
+            "<span class= 'gras'>Magasins : </span>" +
             resultat.products[0]["stores"] +
             "<br/><br/>";
     }
 
     if (resultat.products[0]["conservation_conditions_fr"]) {
         texte +=
-            "<span class= 'gras' >Conservation : </span>" +
+            "<span class= 'gras'>Conservation : </span>" +
             resultat.products[0]["conservation_conditions_fr"] +
             "<br/><br/>";
     }
